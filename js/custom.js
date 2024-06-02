426bfb8939da27c5d7302c3f2fb4ac3b325d344d
zee78 <zeeshanmasood93@yahoo.com>
2024-06-02 22:46:00 +0500

    landing page design with responsiveness

---
+++ js/custom.js
@@ -0,0 +1,392 @@
+var slider = tns({
+    container: '#serviceCarousel',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: false,
+    controlsPosition: 'top',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    gutter: 20,
+    arrowKeys: false,
+    freezable: false,
+    controlsContainer: '#service-controls',
+    responsive: {
+        640: {
+            items: 1,
+        },
+        768: {
+            items: 2,
+        },
+        850: {
+            items: 2,
+        },
+        1100: {
+            items: 3,
+        }
+    }
+});
+var slider = tns({
+    container: '#caringCarousel',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: false,
+    controlsPosition: 'top',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    gutter: 20,
+    arrowKeys: false,
+    controlsContainer: '#caring-controls',
+    responsive: {
+        640: {
+            items: 1,
+        },
+        768: {
+            items: 2,
+        },
+        850: {
+            items: 2,
+        },
+        1100: {
+            items: 3,
+        }
+    }
+});
+
+var slider = tns({
+    container: '#artisticCarousel',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: false,
+    controlsPosition: 'top',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    gutter: 20,
+    arrowKeys: false,
+    controlsContainer: '#artistic-controls',
+    responsive: {
+        640: {
+            items: 1,
+        },
+        768: {
+            items: 2,
+        },
+        850: {
+            items: 2,
+        },
+        1100: {
+            items: 3,
+        }
+    }
+});
+
+var slider = tns({
+    container: '#cookingCarousel',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: false,
+    controlsPosition: 'top',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    gutter: 20,
+    arrowKeys: false,
+    controlsContainer: '#cooking-controls',
+    responsive: {
+        640: {
+            items: 1,
+        },
+        768: {
+            items: 2,
+        },
+        850: {
+            items: 2,
+        },
+        1100: {
+            items: 3,
+        }
+    }
+});
+
+var slider = tns({
+    container: '#avialableHomeWorkCarousel',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: false,
+    controlsPosition: 'top',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    gutter: 20,
+    arrowKeys: false,
+    controlsContainer: '#avialableHomeWork-controls',
+    responsive: {
+        640: {
+            items: 1,
+        },
+        768: {
+            items: 2,
+        },
+        850: {
+            items: 2,
+        },
+        1100: {
+            items: 3,
+        }
+    }
+});
+
+var slider = tns({
+    container: '#availableCaringCarousel',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: false,
+    controlsPosition: 'top',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    gutter: 20,
+    arrowKeys: false,
+    controlsContainer: '#availableCaring-controls',
+    responsive: {
+        640: {
+            items: 1,
+        },
+        768: {
+            items: 2,
+        },
+        850: {
+            items: 2,
+        },
+        1100: {
+            items: 3,
+        }
+    }
+});
+
+var slider = tns({
+    container: '#availableArtisticCarousel',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: false,
+    controlsPosition: 'top',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    gutter: 20,
+    arrowKeys: false,
+    controlsContainer: '#availableArtistic-controls',
+    responsive: {
+        640: {
+            items: 1,
+        },
+        768: {
+            items: 2,
+        },
+        850: {
+            items: 2,
+        },
+        1100: {
+            items: 3,
+        }
+    }
+});
+var slider = tns({
+    container: '#availableCookingCarousel',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: false,
+    controlsPosition: 'top',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    gutter: 20,
+    arrowKeys: false,
+    controlsContainer: '#availableCooking-controls',
+    responsive: {
+        640: {
+            items: 1,
+        },
+        768: {
+            items: 2,
+        },
+        850: {
+            items: 2,
+        },
+        1100: {
+            items: 3,
+        }
+    }
+});
+
+var slider = tns({
+    container: '#professionalCarousel',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: false,
+    controlsPosition: 'top',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    arrowKeys: false,
+    controlsContainer: '#professional-controls',
+    responsive: {
+        640: {
+            items: 2,
+        },
+        768: {
+            items: 2,
+        },
+        992: {
+            items: 3,
+        },
+        1300: {
+            items: 4,
+        }
+    }
+});
+var slider = tns({
+    container: '#professionalCaringCarousel',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: false,
+    controlsPosition: 'top',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    arrowKeys: true,
+    controlsContainer: '#professionalCaring-controls',
+    responsive: {
+        640: {
+            items: 2,
+        },
+        768: {
+            items: 2,
+        },
+        992: {
+            items: 3,
+        },
+        1300: {
+            items: 4,
+        }
+    }
+});
+
+var slider = tns({
+    container: '#professionalArtisticCarousel',
+    items: 4,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: false,
+    controlsPosition: 'top',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    arrowKeys: true,
+    controlsContainer: '#professionalArtistic-controls',
+    responsive: {
+        640: {
+            items: 2,
+        },
+        768: {
+            items: 2,
+        },
+        992: {
+            items: 3,
+        },
+        1300: {
+            items: 4,
+        }
+    }
+});
+
+var slider = tns({
+    container: '#professionalCookingCarousel',
+    items: 4,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: false,
+    controlsPosition: 'top',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    arrowKeys: true,
+    controlsContainer: '#professionalCooking-controls',
+    responsive: {
+        640: {
+            items: 2,
+        },
+        768: {
+            items: 2,
+        },
+        992: {
+            items: 3,
+        },
+        1300: {
+            items: 4,
+        }
+    }
+});
+
+var slider = tns({
+    container: '#customerCarousel',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    speed: 5000,
+    nav: true,
+    controlsPosition: 'bottom',
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    loop: true,
+    arrowKeys: true,
+    gutter: 25,
+    controlsContainer: '#customer-controls',
+    navContainer: false,
+    responsive: {
+        640: {
+            items: 1,
+        },
+        
+        992: {
+            items: 2,
+        }
+    }
+});
